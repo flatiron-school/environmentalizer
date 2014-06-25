53a2d899e645bcefb0d2bcca9ecfae579088e710
Logan Hasson <logan.hasson@gmail.com>
2014-06-25 17:56:49 -0400

    Download .bash_profile

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+
+function copyBashProfile {
+  echo 'Getting Flatiron School .bash_profile...'
+  cd ~
+  
+  if [ -f .bash_profile ]; then
+    mv .bash_profile .bash_profile.old
+  fi
+
+  curl "http://bit.ly/flatiron-school-bash-profile" -o ".bash_profile"
+}
+
+# copyBashProfile
