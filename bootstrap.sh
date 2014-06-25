16178796164571e818e5cd24053c06a8aab2d8ea
Logan Hasson <logan.hasson@gmail.com>
2014-06-25 18:44:25 -0400

    Install command line tools

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,4 +11,27 @@ function copyBashProfile {
   curl "http://bit.ly/flatiron-school-bash-profile" -o ".bash_profile"
 }
 
+function getCommandLineTools {
+  echo 'Downloading command line tools...'
+  cd ~
+
+  if [ -f cli_tools.dmg ]; then
+    rm cli_tools.dmg
+  fi
+
+  curl "http://bit.ly/flatiron-gcc" -o "cli_tools.dmg"
+}
+
+function installCommandLineTools {
+  echo 'Installing command line tools...'
+  cd ~
+
+  hdiutil attach cli_tools.dmg
+  installer -pkg "/Volumes/Command Line Developer Tools/Command Line Tools (OS X 10.9).pkg" -target "/Volumes/Macintosh HD"
+  hdiutil detach "/Volumes/Command Line Developer Tools"
+  rm cli_tools.dmg
+}
+
 # copyBashProfile
+# getCommandLineTools
+# installCommandLineTools
