14f5d03515a0ec1d51d0a80ed99784643e745a39
Logan Hasson <logan.hasson@gmail.com>
2014-06-26 15:31:46 -0400

    Add script to edit sudoers file temporarily

---
+++ edit_sudoers.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd ~
+
+sudo cp /etc/sudoers /etc/sudoers.bak
+sudo cp /etc/sudoers /etc/sudoers.tmp
+
+sudo chmod 0640 /etc/sudoers.tmp
+sudo echo "$USER ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers.tmp
+sudo chmod 0440 /etc/sudoers.tmp
+
+sudo mv /etc/sudoers.tmp /etc/sudoers
+
+exit 0
