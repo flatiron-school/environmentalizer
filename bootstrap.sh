f247c1946f8faea63036e955a44b3c01045e0923
Logan Hasson <logan.hasson@gmail.com>
2014-06-26 14:44:01 -0400

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -80,7 +80,7 @@ function installSublime {
   cd ~
 
   hdiutil attach sublime.dmg
-  cp -r "/Volumes/Sublime Text 2/Sublime Text 2.app" "$HOME/Applications/Sublime Text 2.app"
+  cp -r "/Volumes/Sublime Text 2/Sublime Text 2.app" "/Applications/Sublime Text 2.app"
   hdiutil detach "/Volumes/Sublime Text 2"
 
   rm sublime.dmg
