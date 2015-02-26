0b53261d8f11dffbcf80a8c2bbf1f09a4c46656d
Logan Hasson <logan.hasson@gmail.com>
2015-02-26 12:38:25 -0500

    Check for sqlite3

--- 10.10/sqlite3/check.sh
+++ 10.10/sqlite3/check.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 echo 'Checking SQLite3...'
+
+if [ -n "$(which sqlite3)" ] && [ -f '/usr/bin/sqlite3' ]; then
+  if [[ $(sqlite3 --version) =~ ^3\.8.*$ ]]; then
+    echo 'installed'
+  fi
+fi
