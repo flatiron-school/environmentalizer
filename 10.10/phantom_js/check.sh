ba02f88ebd7e1ba80e0de23415329405602081c0
Logan Hasson <logan.hasson@gmail.com>
2015-02-26 13:21:49 -0500

    Check for phantomjs

--- 10.10/phantom_js/check.sh
+++ 10.10/phantom_js/check.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-echo 'Checking PhantomJS...'
+if [ -n "$(brew ls --versions phantomjs 2> /dev/null)" ] && [[ $(which phantomjs) =~ ^/usr/local/bin/phantomjs$ ]] && [[ $(phantomjs --version 2> /dev/null) =~ ^1\.9\.[7-9]$ ]]; then
+  echo 'installed'
+fi
