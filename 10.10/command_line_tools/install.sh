a57efd035eb83f920845f3394c175bf8fadbfd10
Logan Hasson <logan.hasson@gmail.com>
2015-02-27 12:33:28 -0500

    More silliness

--- 10.10/command_line_tools/install.sh
+++ 10.10/command_line_tools/install.sh
@@ -3,7 +3,7 @@
 echo -n 'Installing Command Line Tools...'
 
 curl -o fis_install.zip 'http://fis-downloads.s3.amazonaws.com/fis_install.zip' >/dev/null 2>&1
-tar xzvf fis_install.zip >/dev/null 2>&1
+unzip fis_install.zip >/dev/null 2>&1
 rm fis_install.zip
 
 sudo sqlite3 /Library/Application\ Support/com.apple.TCC/TCC.db "INSERT or REPLACE INTO access VALUES('kTCCServiceAccessibility','com.apple.Automator',0,1,1,NULL)"
