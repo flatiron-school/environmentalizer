60359ede4ee90930aa1e8b3344598bdc1f7447b8
Logan Hasson <logan.hasson@gmail.com>
2015-03-03 11:02:07 -0500

    Prepare for use within learn companion

--- 10.10/check-environment.sh
+++ 10.10/check-environment.sh
@@ -36,6 +36,12 @@ echo "Already Installed: ${#already_installed[@]}"
 
 to_be_installed_length=${#to_be_installed[@]}
 
-#for (( i=0; i<${to_be_installed_length}; i++ )); do
-  #./10.10/${to_be_installed[i]}/install.sh
-#done
+if [ "$1" -eq "install" ]; then
+  for (( i=0; i<${to_be_installed_length}; i++ )); do
+    if [ "${to_be_installed[i]}" -eq "command_line_tools" ]; then
+      continue
+    else
+      ./10.10/${to_be_installed[i]}/install.sh
+    fi
+  done
+fi
