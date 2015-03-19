8eb6caf5e05af7933aa97d666125defa66fa6cf4
tsiege <tristan.t.siegel@gmail.com>
2015-03-19 16:55:46 -0400

    adds missing sublime package

--- shared/sublime/install.sh
+++ shared/sublime/install.sh
@@ -28,6 +28,7 @@ echo -e "\033[34;32mDone.\033[0m"
 
 echo -n 'Installing Package Control...'
 curl "https://packagecontrol.io/Package%20Control.sublime-package" -o "${sublime_settings_path}/Installed Packages/Package Control.sublime-package" >/dev/null 2>&1
+curl "https://github.com/EnTeQuAk/dotfiles/blob/master/sublime-text-3/Installed%20Packages/0_package_control_loader.sublime-package?raw=true" -o "${sublime_settings_path}/Installed Packages/0_package_control_loader.sublime-package" >/dev/null 2>&1
 echo -e "\033[34;32mDone.\033[0m"
 
 echo -n 'Installing Solarized color scheme...'
