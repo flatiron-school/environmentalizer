d6076e4af3d30748a4927467ce52f0280a476bf4
tsiege <tristan.t.siegel@gmail.com>
2015-03-19 14:24:25 -0400

    adds newline

--- shared/sublime_settings/check.sh
+++ shared/sublime_settings/check.sh
@@ -5,7 +5,8 @@ sublime_three_settings="$HOME/Library/Application Support/Sublime Text 3/Package
 
 echo 'Checking Sublime Text Settings...'
 
-if [ -f $sublime_two_settings ] && [ -f $sublime_three_settings ]; then
+if [ -f $sublime_two_settings ] && \
+   [ -f $sublime_three_settings ]; then
   if [ -n "$(cat "${sublime_two_settings}" | grep '\"tab_size\": 2')" ] && \
      [ -n "$(cat "${sublime_two_settings}" | grep '\"translate_tabs_to_spaces\": true')" ] || \
      [ -n "$(cat "${sublime_three_settings}" | grep '\"tab_size\": 2')" ] && \
