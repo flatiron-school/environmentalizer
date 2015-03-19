4ba5d4104210dd406e5f91c62974e2aaa0742884
tsiege <tristan.t.siegel@gmail.com>
2015-03-19 14:13:00 -0400

    removes sublime 3 preferences check

--- shared/sublime_settings/check.sh
+++ shared/sublime_settings/check.sh
@@ -5,9 +5,7 @@ sublime_three_settings="$HOME/Library/Application Support/Sublime Text 3/Package
 
 echo 'Checking Sublime Text Settings...'
 if [ -n "$(cat $HOME/Library/Application Support/Sublime Text 2/Packages/Default/Preferences.sublime-settings | grep '\"tab_size\": 2')" ] && \
-   [ -n "$(cat $HOME/Library/Application Support/Sublime Text 2/Packages/Default/Preferences.sublime-settings | grep '\"translate_tabs_to_spaces\": true')" ] || \
-   [ -n "$(cat $HOME/Library/Application Support/Sublime Text 3/Packages/User/Preferences.sublime-settings | grep '\"tab_size\": 2')" ] && \
-   [ -n "$(cat $HOME/Library/Application Support/Sublime Text 3/Packages/User/Preferences.sublime-settings | grep '\"translate_tabs_to_spaces\": true')" ]
+   [ -n "$(cat $HOME/Library/Application Support/Sublime Text 2/Packages/Default/Preferences.sublime-settings | grep '\"translate_tabs_to_spaces\": true')" ]
 then
   echo 'installed'
 fi
