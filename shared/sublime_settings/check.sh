074f636c8a24d37f7be0ff10ad27cc8308a92256
tsiege <tristan.t.siegel@gmail.com>
2015-03-19 14:23:04 -0400

    makes sublime preference checker more robust

--- shared/sublime_settings/check.sh
+++ shared/sublime_settings/check.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+sublime_two_settings="$HOME/Library/Application Support/Sublime Text 2/Packages/Default/Preferences.sublime-settings"
+sublime_three_settings="$HOME/Library/Application Support/Sublime Text 3/Packages/User/Preferences.sublime-settings"
+
 echo 'Checking Sublime Text Settings...'
-if [ -n "$(cat "$HOME/Library/Application Support/Sublime Text 2/Packages/Default/Preferences.sublime-settings" | grep '\"tab_size\": 2')" ] && \
-   [ -n "$(cat "$HOME/Library/Application Support/Sublime Text 2/Packages/Default/Preferences.sublime-settings" | grep '\"translate_tabs_to_spaces\": true')" ]
-then
-  echo 'installed'
+
+if [ -f $sublime_two_settings ] && [ -f $sublime_three_settings ]; then
+  if [ -n "$(cat "${sublime_two_settings}" | grep '\"tab_size\": 2')" ] && \
+     [ -n "$(cat "${sublime_two_settings}" | grep '\"translate_tabs_to_spaces\": true')" ] || \
+     [ -n "$(cat "${sublime_three_settings}" | grep '\"tab_size\": 2')" ] && \
+     [ -n "$(cat "${sublime_three_settings}" | grep '\"translate_tabs_to_spaces\": true')" ]
+  then
+    echo 'installed'
+  fi
 fi
