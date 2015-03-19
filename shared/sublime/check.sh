03ee70ec6f83891c2e2a0d8aeedd7eb2b15bf6d0
tsiege <tristan.t.siegel@gmail.com>
2015-03-19 12:36:53 -0400

    upgrades from sublime text 2 to 3

--- shared/sublime/check.sh
+++ shared/sublime/check.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-result=$(mdfind "kind:app sublime")
+result=$(mdfind "kind:app sublime text")
 
 if [[ -n $result ]]; then
   echo 'installed'
--- shared/sublime/install.sh
+++ shared/sublime/install.sh
@@ -6,29 +6,32 @@ if [ -f sublime.dmg ]; then
   rm sublime.dmg
 fi
 
+sublime_settings_path="$HOME/Library/Application Support/Sublime Text 3/"
+sublime_color_theme_path="${sublime_color_theme_path}"
 
 echo -n 'Downloading...'
-curl "http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.2.dmg" -o "sublime.dmg" >/dev/null 2>&1
+curl "http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%20Build%203065.dmg" -o "sublime.dmg" >/dev/null 2>&1
 echo -e "\033[34;32mDone.\033[0m"
 
 echo -n 'Installing...'
 hdiutil attach sublime.dmg > /dev/null
-cp -r "/Volumes/Sublime Text 2/Sublime Text 2.app" "/Applications/Sublime Text 2.app"
-hdiutil detach "/Volumes/Sublime Text 2" > /dev/null
+cp -r "/Volumes/Sublime Text/Sublime Text.app" "/Applications/Sublime Text.app"
+hdiutil detach "/Volumes/Sublime Text" > /dev/null
 rm sublime.dmg
 echo -e "\033[34;32mDone.\033[0m"
 
 echo -n 'Setting up symlink...'
-sudo ln -s "/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl" /usr/local/bin
-open "/Applications/Sublime Text 2.app" && sleep 3 && killall "Sublime Text 2"
+sudo ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" /usr/local/bin
+open "/Applications/Sublime Text.app" && sleep 3 && killall "Sublime Text"
 echo -e "\033[34;32mDone.\033[0m"
 
 echo -n 'Installing Package Control...'
-curl "https://sublime.wbond.net/Package%20Control.sublime-package" -o "$HOME/Library/Application Support/Sublime Text 2/Installed Packages/Package Control.sublime-package" >/dev/null 2>&1
+curl "https://packagecontrol.io/Package%20Control.sublime-package" -o "${sublime_settings_path}/Installed Packages/Package Control.sublime-package" >/dev/null 2>&1
 echo -e "\033[34;32mDone.\033[0m"
 
 echo -n 'Installing Solarized color scheme...'
-curl "http://flatironschool.s3.amazonaws.com/curriculum/resources/environment/themes/Solarized%20Flatiron.zip" -o "$HOME/Library/Application Support/Sublime Text 2/Packages/Color Scheme - Default/Solarized Flatiron.zip" >/dev/null 2>&1
-tar -zxvf "$HOME/Library/Application Support/Sublime Text 2/Packages/Color Scheme - Default/Solarized Flatiron.zip"
-rm "$HOME/Library/Application Support/Sublime Text 2/Packages/Color Scheme - Default/Solarized Flatiron.zip"
+mkdir "${sublime_color_theme_path}"
+curl "http://flatironschool.s3.amazonaws.com/curriculum/resources/environment/themes/Solarized%20Flatiron.zip" -o "${sublime_color_theme_path}/Solarized Flatiron.zip" >/dev/null 2>&1
+tar -zxvf "${sublime_color_theme_path}/Solarized Flatiron.zip"
+rm "${sublime_settings_path}/Solarized Flatiron.zip" "${sublime_settings_path}/Solarized Dark (Flatiron).terminal" "${sublime_settings_path}/Solarized Light (Flatiron).terminal"
 echo -e "\033[34;32mDone.\033[0m"
