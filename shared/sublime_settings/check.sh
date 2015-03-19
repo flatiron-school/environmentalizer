f1de1c715daef6f31793b8aebecfdcef7b504c77
tsiege <tristan.t.siegel@gmail.com>
2015-03-19 14:14:37 -0400

    reverts sublime preferences check

--- shared/sublime_settings/check.sh
+++ shared/sublime_settings/check.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-sublime_two_settings="$HOME/Library/Application Support/Sublime Text 2/Packages/Default/Preferences.sublime-settings"
-sublime_three_settings="$HOME/Library/Application Support/Sublime Text 3/Packages/User/Preferences.sublime-settings"
-
 echo 'Checking Sublime Text Settings...'
-if [ -n "$(cat $HOME/Library/Application Support/Sublime Text 2/Packages/Default/Preferences.sublime-settings | grep '\"tab_size\": 2')" ] && \
-   [ -n "$(cat $HOME/Library/Application Support/Sublime Text 2/Packages/Default/Preferences.sublime-settings | grep '\"translate_tabs_to_spaces\": true')" ]
+if [ -n "$(cat "$HOME/Library/Application Support/Sublime Text 2/Packages/Default/Preferences.sublime-settings" | grep '\"tab_size\": 2')" ] && \
+   [ -n "$(cat "$HOME/Library/Application Support/Sublime Text 2/Packages/Default/Preferences.sublime-settings" | grep '\"translate_tabs_to_spaces\": true')" ]
 then
   echo 'installed'
 fi
