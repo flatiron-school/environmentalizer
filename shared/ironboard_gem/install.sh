47fe9589901773a62c77fe0cdbaeaa0718dd2130
tsiege <tristan.t.siegel@gmail.com>
2015-03-23 16:50:45 -0400

    tries to fix rvm install error

--- shared/ironboard_gem/install.sh
+++ shared/ironboard_gem/install.sh
@@ -2,8 +2,7 @@
 
 echo 'Installing Ironboard gem...'
 
-[[ -s "/Users/$USER/.rvm/scripts/rvm" ]] && source "/Users/$USER/.rvm/scripts/rvm"
-gem sources -a http://flatiron:33west26@gems.flatironschool.com
+# gem sources -a http://flatiron:33west26@gems.flatironschool.com
 gem install ironboard
 
 echo -e "\033[34;32mDone.\033[0m"
--- shared/rvm/install.sh
+++ shared/rvm/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo 'Installing RVM...'
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+# gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 \curl -sSL https://get.rvm.io | bash -s stable
 [[ -s "/Users/$USER/.rvm/scripts/rvm" ]] && source "/Users/$USER/.rvm/scripts/rvm"
 echo -e "\033[34;32mDone.\033[0m"
