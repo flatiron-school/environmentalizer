64e0c364abc2ea9544b5c43030401293f8b99366
tsiege <tristan.t.siegel@gmail.com>
2015-03-23 16:58:10 -0400

    fixes typo

--- shared/ironboard_gem/install.sh
+++ shared/ironboard_gem/install.sh
@@ -2,7 +2,7 @@
 
 echo 'Installing Ironboard gem...'
 
-# gem sources -a http://flatiron:33west26@gems.flatironschool.com
+gem sources -a http://flatiron:33west26@gems.flatironschool.com
 gem install ironboard
 
 echo -e "\033[34;32mDone.\033[0m"
