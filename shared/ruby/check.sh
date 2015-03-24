25fb8128938d20dbb2e5dabfadcaf9e68198cbfd
tsiege <tristan.t.siegel@gmail.com>
2015-03-24 10:57:00 -0400

    attempts to fix check

--- shared/ruby/check.sh
+++ shared/ruby/check.sh
@@ -2,7 +2,7 @@
 
 if [[ -n $(which rvm) ]] && \
    [[ -n $(rvm list 2>/dev/null | grep ruby-2. )]] || \
-   [[ $(which rvm) -eq '' ]]
+   [[ $(which rvm 2>/dev/null) -eq '' ]]
 then
   echo 'installed'
 fi
