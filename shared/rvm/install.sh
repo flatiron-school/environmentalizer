f53f54cbc38ddd8a91696ceaf2a8cd7330e01253
tsiege <tristan.t.siegel@gmail.com>
2015-03-24 14:25:31 -0400

    adds fix for gnp

--- shared/rvm/install.sh
+++ shared/rvm/install.sh
@@ -3,6 +3,7 @@
 echo 'Installing RVM...'
 
 brew upgrade gmp
+brew unlink gnupg && brew link gnupg
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 \curl -sSL https://get.rvm.io | bash -s stable --ruby
 source $HOME/.rvm/scripts/rvm
