53b7f5a0f94fed03474cd46ce0bed63a051d1b09
tsiege <tristan.t.siegel@gmail.com>
2015-03-24 14:40:19 -0400

    updates how rvm installs ruby

--- shared/rvm/install.sh
+++ shared/rvm/install.sh
@@ -2,10 +2,10 @@
 
 echo 'Installing RVM...'
 
-brew upgrade gmp
-brew unlink gnupg && brew link gnupg
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-\curl -sSL https://get.rvm.io | bash -s stable --ruby
+brew upgrade gmp 2> /dev/null
+brew unlink gnupg && brew link gnupg 1> /dev/null
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 2>/dev/null
+\curl -sSL https://get.rvm.io | bash -s stable
 source $HOME/.rvm/scripts/rvm
 
 echo -e "\033[34;32mDone.\033[0m"
