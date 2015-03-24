cbd70172fb67d4bb3befc861ee663b4d1ac339c0
tsiege <tristan.t.siegel@gmail.com>
2015-03-24 14:50:51 -0400

    fixes typo

--- shared/rvm/install.sh
+++ shared/rvm/install.sh
@@ -5,7 +5,7 @@ echo 'Installing RVM...'
 brew upgrade gmp 2> /dev/null
 brew unlink gnupg && brew link gnupg 1> /dev/null
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 2>/dev/null
-\curl -sSL https://get.rvm.io | bash -s stable
+\curl -sSL https://get.rvm.io | bash
 source $HOME/.rvm/scripts/rvm
 
 echo -e "\033[34;32mDone.\033[0m"
