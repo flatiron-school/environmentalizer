553cb94b48c2d133d533da88da863e32219f8088
tsiege <tristan.t.siegel@gmail.com>
2015-03-24 11:16:46 -0400

    adds fix for missing libraries w/ rvm

--- shared/rvm/install.sh
+++ shared/rvm/install.sh
@@ -2,6 +2,7 @@
 
 echo 'Installing RVM...'
 
+brew upgrade gmp
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 \curl -sSL https://get.rvm.io | bash -s stable --ruby
 source $HOME/.rvm/scripts/rvm
