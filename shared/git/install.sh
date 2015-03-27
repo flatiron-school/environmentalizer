c5f46995ff87a288468c5db7a2885f974ff423d2
tsiege <tristan.t.siegel@gmail.com>
2015-03-27 16:54:26 -0400

    silences errors on git install

--- shared/git/install.sh
+++ shared/git/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo 'Installing git...'
-brew install git || brew update 1>&2 2>/dev/null && brew upgrade git
+brew install git || brew update 1>&2 2>/dev/null && brew upgrade git 2>/dev/null
 echo -e "\033[34;32mDone.\033[0m"
