625ff2cec9f7755beb0d37cd0d8bb5b99105346f
tsiege <tristan.t.siegel@gmail.com>
2015-03-27 10:32:29 -0400

    silences errors from checking for sublime settings

--- shared/sublime_settings/check.sh
+++ shared/sublime_settings/check.sh
@@ -4,10 +4,10 @@ sublime_two_settings="$HOME/Library/Application Support/Sublime Text 2/Packages/
 sublime_three_settings="$HOME/Library/Application Support/Sublime Text 3/Packages/User/Preferences.sublime-settings"
 
 if [ -f "${sublime_two_settings}" ] || [ -f "${sublime_three_settings}" ]; then
-  if [ -n "$(cat "${sublime_two_settings}" | grep '\"tab_size\": 2')" ] && \
-     [ -n "$(cat "${sublime_two_settings}" | grep '\"translate_tabs_to_spaces\": true')" ] || \
-     [ -n "$(cat "${sublime_three_settings}" | grep '\"tab_size\": 2')" ] && \
-     [ -n "$(cat "${sublime_three_settings}" | grep '\"translate_tabs_to_spaces\": true')" ]
+  if [ -n "$(cat "${sublime_two_settings}" 2>/dev/null | grep '\"tab_size\": 2')" ] && \
+     [ -n "$(cat "${sublime_two_settings}" 2>/dev/null | grep '\"translate_tabs_to_spaces\": true')" ] || \
+     [ -n "$(cat "${sublime_three_settings}" 2>/dev/null | grep '\"tab_size\": 2')" ] && \
+     [ -n "$(cat "${sublime_three_settings}" 2>/dev/null | grep '\"translate_tabs_to_spaces\": true')" ]
   then
     echo 'installed'
   fi
