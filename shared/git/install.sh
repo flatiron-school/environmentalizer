83f60c0e599e0dc1803aa96886df2ee9d007362d
tsiege <tristan.t.siegel@gmail.com>
2015-03-27 17:05:54 -0400

    augments try catch with installing git

--- shared/git/install.sh
+++ shared/git/install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 echo 'Installing git...'
-brew install git || brew update 1>&2 2>/dev/null && brew upgrade git 2>/dev/null
+{ # try
+  brew install git 2>/dev/null
+} || { # catch
+  brew update 1>&2 2>/dev/null && brew upgrade git 2>/dev/null
+}
 echo -e "\033[34;32mDone.\033[0m"
