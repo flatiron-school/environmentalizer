a98cac93e4c94b15c16822dd065291d84292d3c2
tsiege <tristan.t.siegel@gmail.com>
2015-03-27 10:30:44 -0400

    fixes sublime settings check

--- shared/sublime_settings/check.sh
+++ shared/sublime_settings/check.sh
@@ -3,9 +3,7 @@
 sublime_two_settings="$HOME/Library/Application Support/Sublime Text 2/Packages/Default/Preferences.sublime-settings"
 sublime_three_settings="$HOME/Library/Application Support/Sublime Text 3/Packages/User/Preferences.sublime-settings"
 
-echo 'Checking Sublime Text Settings...'
-
-if [ -f "${sublime_two_settings}" ] && [ -f "${sublime_three_settings}" ]; then
+if [ -f "${sublime_two_settings}" ] || [ -f "${sublime_three_settings}" ]; then
   if [ -n "$(cat "${sublime_two_settings}" | grep '\"tab_size\": 2')" ] && \
      [ -n "$(cat "${sublime_two_settings}" | grep '\"translate_tabs_to_spaces\": true')" ] || \
      [ -n "$(cat "${sublime_three_settings}" | grep '\"tab_size\": 2')" ] && \
