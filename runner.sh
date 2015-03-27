357236302ec725c99198b7f5cb180aaba7cd85ab
tsiege <tristan.t.siegel@gmail.com>
2015-03-27 09:34:00 -0400

    makes zip dl more specific

--- runner.sh
+++ runner.sh
@@ -6,13 +6,13 @@ echo 'Determining OS X version...'
 if [[ $os_version =~ ^10\.9|10\.[0-9]*$ ]] || [[ $os_version =~ ^10\.9|10$ ]]; then
   echo 'Downloading source code...'
   cd ~
-  curl -L "https://github.com/flatiron-labs/environmentalizer/archive/master.zip" > master.zip
-  unzip -qq master.zip 1>&2 2>/dev/null
+  curl -L "https://github.com/flatiron-labs/environmentalizer/archive/master.zip" > "$HOME/environmentalizer.zip"
+  unzip -qq "$HOME/environmentalizer.zip" 1>&2 2>/dev/null
   cd environmentalizer-master
   . ./shared/bootstrap.sh
   cd ..
   rm -rf environmentalizer-master
-  rm master.zip
+  rm environmentalizer.zip
 else
   echo 'Not a supported OS X version'
   exit 1
