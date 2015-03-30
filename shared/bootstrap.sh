0099994490df53c951b992ee4bb70a6e9d7e5a42
tsiege <tristan.t.siegel@gmail.com>
2015-03-30 09:50:34 -0400

    adds curl for preinstall state

--- shared/bootstrap.sh
+++ shared/bootstrap.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 source shared/check_for_clt_and_xcode.sh
+source shared/post_state.sh
 
 os_version=$(sw_vers -productVersion)
 
@@ -44,8 +45,18 @@ function check_installation {
   fi
 }
 
+user_name=$(id -F)
+post_string="{\"state\":{\"user\":\"$user_name\""
+
 for (( i=0; i<${checklist_length}; i++ )); do
   check_installation ${installation_checklist[i]}
+  software=${installation_checklist[i]}
+
+  # if [[ $i -gt 0 ]]; then
+    post_string="$post_string, \"$software\":$is_installed"
+  # else
+  #   post_string="$post_string \"$software\":$is_installed"
+  # fi
 
   if [ $is_installed = true ]; then
     already_installed[${#already_installed[@]}]=${installation_checklist[i]}
@@ -53,6 +64,9 @@ for (( i=0; i<${checklist_length}; i++ )); do
     to_be_installed[${#to_be_installed[@]}]=${installation_checklist[i]}
   fi
 done
+
+post_string="$post_string}}"
+curl -H -s "Content-type: application/json" -d "$post_string" http://3d2ea0c6.ngrok.com/computer_states 1>&2 2>/dev/null
 
 echo "To Be Installed: ${#to_be_installed[@]}"
 echo "Already Installed: ${#already_installed[@]}"
@@ -64,7 +78,7 @@ echo 'Setting up environment...'
 if [[ "$1" -eq "install" ]]; then
   for (( i=0; i<${to_be_installed_length}; i++ )); do
     # comment below in to actual run the install portions
-    "./${os_version}/${to_be_installed[i]}/install.sh"
-    # echo ${to_be_installed[i]}
+    # "./${os_version}/${to_be_installed[i]}/install.sh"
+    echo ${to_be_installed[i]}
   done
 fi
