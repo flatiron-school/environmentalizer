80f52eccce40fc72bd5b87a2fe054338b2d674e2
tsiege <tristan.t.siegel@gmail.com>
2015-03-30 10:15:43 -0400

    hotfix for git

--- shared/git/install.sh
+++ shared/git/install.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
 echo 'Installing git...'
-{ # try
-  brew install git 2>/dev/null &&
-} || { # catch
-  brew update 1>&2 2>/dev/null && brew upgrade git 2>/dev/null
-}
+  brew install git 2>/dev/null || brew update 1>&2 2>/dev/null && brew upgrade git 2>/dev/null
 echo -e "\033[34;32mDone.\033[0m"
