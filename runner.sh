7eb99c21c53b09f132a6fb8a81907cfd7c95bdde
tsiege <tristan.t.siegel@gmail.com>
2015-03-30 09:55:53 -0400

    alters branch in curl for testing

--- runner.sh
+++ runner.sh
@@ -6,12 +6,12 @@ echo 'Determining OS X version...'
 if [[ $os_version =~ ^10\.9|10\.[0-9]*$ ]] || [[ $os_version =~ ^10\.9|10$ ]]; then
   echo 'Downloading source code...'
   cd ~
-  curl -L "https://github.com/flatiron-labs/environmentalizer/archive/master.zip" > "$HOME/environmentalizer.zip"
+  curl -L "https://github.com/flatiron-labs/environmentalizer/archive/curl.zip" > "$HOME/environmentalizer.zip"
   unzip -qq "$HOME/environmentalizer.zip" 1>&2 2>/dev/null
-  cd environmentalizer-master
+  cd environmentalizer-curl
   . ./shared/bootstrap.sh
   cd ..
-  rm -rf environmentalizer-master
+  rm -rf environmentalizer-curl
   rm environmentalizer.zip
 else
   echo 'Not a supported OS X version'
