36648076d501f71f281bfb8d4f9c7919f695448f
tsiege <tristan.t.siegel@gmail.com>
2015-04-01 11:45:00 -0400

    fixes where runner scources zip file from

--- runner.sh
+++ runner.sh
@@ -6,7 +6,7 @@ echo 'Determining OS X version...'
 if [[ $os_version =~ ^10\.9|10\.[0-9]*$ ]] || [[ $os_version =~ ^10\.9|10$ ]]; then
   echo 'Downloading source code...'
   cd ~
-  curl -L "https://github.com/flatiron-labs/environmentalizer/archive/master.zip" > "$HOME/environmentalizer.zip"
+  curl -L "https://github.com/flatiron-school/environmentalizer/archive/master.zip" > "$HOME/environmentalizer.zip"
   unzip -qq "$HOME/environmentalizer.zip" 1>&2 2>/dev/null
   cd environmentalizer-master
   . ./shared/bootstrap.sh
