f073ca6641f5d01233d2fb0dae3a7c352dcd4c12
tsiege <tristan.t.siegel@gmail.com>
2015-04-06 13:39:26 -0400

    fixes how learn gem version is captured

--- shared/learn_gem/version.sh
+++ shared/learn_gem/version.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 function learn_gem_version {
   if $is_installed ; then
-    echo $(cat $HOME/.rvm/gems/ruby-2.2.0/gems/learn-co-1.0.2/lib/learn/version.rb | perl -lne 'print $& if /\d.*[^"]/')
+    echo $(gem list | grep learn | perl -lne 'print $& if /(\d.*)[^\)]/')
   else
     echo false
   fi
