e0c0b3e5194a321b18f3c75c80a7e5cc44af7e18
tsiege <tristan.t.siegel@gmail.com>
2015-04-06 17:25:39 -0400

    reverts runner to point to master

--- runner.sh
+++ runner.sh
@@ -6,12 +6,12 @@ echo 'Determining OS X version...'
 if [[ $os_version =~ ^10\.9|10\.[0-9]*$ ]] || [[ $os_version =~ ^10\.9|10$ ]]; then
   echo 'Downloading source code...'
   cd ~
-  curl -L "https://github.com/flatiron-school/environmentalizer/archive/versions.zip" > "$HOME/environmentalizer.zip"
+  curl -L "https://github.com/flatiron-school/environmentalizer/archive/master.zip" > "$HOME/environmentalizer.zip"
   unzip -qq "$HOME/environmentalizer.zip" 1>&2 2>/dev/null
-  cd environmentalizer-versions
+  cd environmentalizer-master
   . ./shared/bootstrap.sh
   cd ..
-  rm -rf environmentalizer-versions
+  rm -rf environmentalizer-master
   rm environmentalizer.zip
 else
   echo "\033[1;31mNot a supported OS X version\033[0m"
