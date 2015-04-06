5841e1f71f987a22d882475e4d9ce6312cde013c
tsiege <tristan.t.siegel@gmail.com>
2015-04-06 11:30:55 -0400

    makes homebrews version more robust

--- shared/homebrew/version.sh
+++ shared/homebrew/version.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 function homebrew_version {
-  if [[ $is_installed -eq true ]]; then
+  if [[ $(is_homebrew_installed) -eq "installed" ]]; then
     echo $(brew --version | perl -lne 'print $& if /\d.*/')
   else
     echo false
