2a02463009dcbe017b5eeb450a14ded2b36e6e15
Logan Hasson <logan.hasson@gmail.com>
2015-05-01 12:53:26 -0400

    Update ruby installation

--- shared/ruby/install.sh
+++ shared/ruby/install.sh
@@ -7,6 +7,18 @@ echo 'Installing Ruby' > /tmp/learninstall.step
 if [[ -f $HOME/.rvm/scripts/rvm ]]; then
   . $HOME/.rvm/scripts/rvm
 fi
-rvm install ruby-2.2.1
+
+if [[ $# -eq 0 ]]
+then
+  echo "Must supply password"
+  exit 1
+fi
+
+expect <<-DONE
+spawn rvm install ruby-2.2.1
+expect -re "password required"
+send "$1\n"
+expect eof
+DONE
 
 echo -e "\033[34;32mDone.\033[0m"
