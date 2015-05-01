2ae0c0c1c2891c0b07bcb883a555cd3be20e4299
Logan Hasson <logan.hasson@gmail.com>
2015-05-01 12:38:13 -0400

    Update homebrew installation

--- shared/homebrew/install.sh
+++ shared/homebrew/install.sh
@@ -3,5 +3,14 @@
 echo 'Installing Homebrew...'
 echo 'Installing Homebrew' > /tmp/learninstall.step
 
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install -o homebrew.rb
+expect <<- DONE
+spawn ruby homebrew.rb
+expect -re "RETURN"
+send "\n"
+expect eof
+DONE
+
+rm homebrew.rb
+
 echo -e "\033[34;32mDone.\033[0m"
