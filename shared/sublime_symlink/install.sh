313b79ebf50faf47554e9d721db3534f94911643
Logan Hasson <logan.hasson@gmail.com>
2015-05-01 13:06:16 -0400

    Require sudo for setting up symlink

--- shared/sublime_symlink/install.sh
+++ shared/sublime_symlink/install.sh
@@ -3,10 +3,16 @@
 echo -n 'Setting up symlink...'
 echo 'Setting up Sublime Text symlink' > /tmp/learninstall.step
 
+if [ "$EUID" -ne 0 ]
+then
+  echo 'Must be run with sudo'
+  exit 1
+fi
+
 if [[ -d "/Applications/Sublime Text.app" ]]; then
-  sudo ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" /usr/local/bin
+  ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" /usr/local/bin
 elif [[ -d "/Applications/Sublime Text 2.app" ]]; then
-  sudo ln -s "/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl" /usr/local/bin
+  ln -s "/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl" /usr/local/bin
 else
   echo "\033[1;31mSublime Text Symlink could not be created\033[0m"
 fi
