e4d11903a9e88657f6f29cf1f37ea03ef39b1400
Logan Hasson <logan.hasson@gmail.com>
2015-05-02 12:33:16 -0400

    update git check for os x app

--- shared/git/check.sh
+++ shared/git/check.sh
@@ -3,5 +3,7 @@
 if [[ -n "$(brew ls --versions git 2> /dev/null)" ]] &&\
    [[ "$(brew ls --versions git 2> /dev/null)" =~ ^.*2\.2|3.*$ ]]
 then
-  echo 'installed'
+  echo '1'
+else
+  echo '0'
 fi
