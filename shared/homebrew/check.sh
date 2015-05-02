5ad6758968f606cdd3ee403e97b7a8a04b2ff190
Logan Hasson <logan.hasson@gmail.com>
2015-05-02 09:39:15 -0400

    Update homebrew check script output for os x app

--- shared/homebrew/check.sh
+++ shared/homebrew/check.sh
@@ -4,7 +4,9 @@ function is_homebrew_installed {
   if [[ $(which brew) =~ ^/usr/local/bin/brew ]] && \
      [[ $(brew --version 2>/dev/null) =~ ^0\.9\.[5-9]$ ]]
   then
-    echo 'installed'
+    echo '1'
+  else
+    echo '0'
   fi
 }
 
