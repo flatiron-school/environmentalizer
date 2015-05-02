a32870af826063c7314cc2d2db095832e999fb73
Logan Hasson <logan.hasson@gmail.com>
2015-05-02 10:22:13 -0400

    Require password for homebrew

--- shared/homebrew/install.sh
+++ shared/homebrew/install.sh
@@ -3,11 +3,19 @@
 echo 'Installing Homebrew...'
 echo 'Installing Homebrew' > /tmp/learninstall.step
 
+if [[ $# -eq 0 ]]
+then
+  echo "Must supply password"
+  exit 1
+fi
+
 curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install -o homebrew.rb
 expect <<- DONE
 spawn ruby homebrew.rb
 expect -re "RETURN"
 send "\n"
+expect "Password:"
+send "$1\n"
 expect eof
 DONE
 
