60feb7e986b6550cd4eb2b2fd7e3a40edd456dd1
Logan Hasson <logan.hasson@gmail.com>
2015-05-03 10:51:11 -0400

    update git install

--- shared/git/install.sh
+++ shared/git/install.sh
@@ -3,5 +3,5 @@
 echo 'Installing git...'
 echo 'Installing git' > /tmp/learninstall.step
 
-  brew install git 2>/dev/null || brew update 1>&2 2>/dev/null && brew upgrade git 2>/dev/null
+  /usr/local/bin/brew install git 2>/dev/null || /usr/local/bin/brew update 1>&2 2>/dev/null && /usr/local/bin/brew upgrade git 2>/dev/null
 echo -e "\033[34;32mDone.\033[0m"
