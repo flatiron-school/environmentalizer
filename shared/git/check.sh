fdb670fa39490ceced0fcdbab66d32c5f1b602bb
Logan Hasson <logan.hasson@gmail.com>
2015-05-03 10:54:16 -0400

    test removing absolute path to brew in git check

--- shared/git/check.sh
+++ shared/git/check.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [[ -n "$(/usr/local/bin/brew ls --versions git 2> /dev/null)" ]] &&\
-   [[ "$(/usr/local/bin/brew ls --versions git 2> /dev/null)" =~ ^.*2\.4\..*$ ]]
+if [[ -n "$(brew ls --versions git 2> /dev/null)" ]] &&\
+   [[ "$(brew ls --versions git 2> /dev/null)" =~ ^.*2\.4\..*$ ]]
 then
   echo '1'
 else
