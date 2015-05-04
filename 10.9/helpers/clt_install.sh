7fbd334659c6dc166c159d3c67c7e053e6396af5
Logan Hasson <logan.hasson@gmail.com>
2015-05-04 17:56:36 -0400

    update broken clt install script

--- 10.9/helpers/clt_install.sh
+++ 10.9/helpers/clt_install.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 cd ~
 FILE_SIZE=$(curl -sI "http://flatiron-school.s3.amazonaws.com/software/command_line_tools_os_x_mavericks_for_xcode__late_october_2013.dmg" | grep Content-Length | awk '{print $2}')
-echo $FILE_SIZE > clt_install_size_log;
+echo $FILE_SIZE > $HOME/clt_install_size_log;
 
-if [ -f cli_tools.dmg ]; then
-    rm cli_tools.dmg
+if [ -f "$HOME/cli_tools.dmg" ]; then
+    rm $HOME/cli_tools.dmg
 fi
 
-curl --silent "http://flatiron-school.s3.amazonaws.com/software/command_line_tools_os_x_mavericks_for_xcode__late_october_2013.dmg" -o "cli_tools.dmg"
+curl --silent "http://flatiron-school.s3.amazonaws.com/software/command_line_tools_os_x_mavericks_for_xcode__late_october_2013.dmg" -o "$HOME/cli_tools.dmg"
 
-hdiutil attach -nobrowse cli_tools.dmg
+hdiutil attach -nobrowse $HOME/cli_tools.dmg
 
 installer -pkg "/Volumes/Command Line Developer Tools/Command Line Tools (OS X 10.9).pkg" -target "/Volumes/Macintosh HD"
 hdiutil detach "/Volumes/Command Line Developer Tools"
-rm cli_tools.dmg
+rm $HOME/cli_tools.dmg
 
-if [ -f clt_install_size_log ]; then
-    rm clt_install_size_log
+if [ -f "$HOME/clt_install_size_log" ]; then
+    rm $HOME/clt_install_size_log
 fi
 
 echo "DONE"
