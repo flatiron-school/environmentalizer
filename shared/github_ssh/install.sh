8a0ce33d3a7540872e7d94e28e92fb00cfc0c55e
Logan Hasson <logan.hasson@gmail.com>
2015-06-12 13:08:28 -0700

    Update install.sh

--- shared/github_ssh/install.sh
+++ shared/github_ssh/install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 /usr/bin/expect <<EOD
-spawn git clone git@github.com:flatiron-school/environmentalizer.git ~/environmentalizer_learn_ssh_test
+spawn git clone git@github.com:flatiron-school/environmentalizer.git $HOME/environmentalizer_learn_ssh_test
 expect -re "(continue)"
 send "yes\n"
 expect eof
 EOD
 
-rm -rf ~/environmentalizer_learn_ssh_test
+rm -rf $HOME/environmentalizer_learn_ssh_test
 echo -e "\033[34;32mDone.\033[0m"
