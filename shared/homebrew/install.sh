48f2c17d4eb94c91e572e09015e3d285f196b95e
Logan Hasson <logan.hasson@gmail.com>
2015-06-25 10:40:58 -0400

    Update brew installation script

--- shared/homebrew/install.sh
+++ shared/homebrew/install.sh
@@ -8,6 +8,12 @@ then
   exit 1
 fi
 
+# Remove brew's git dir if it exists, otherwise installation will fail
+if [ $(ls /usr/local/.git 2>/dev/null | wc -l) -gt 0 ]
+then
+  rm -rf /usr/local/.git && brew cleanup
+fi
+
 curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install -o $HOME/homebrew.rb
 expect <<- DONE
 spawn ruby $HOME/homebrew.rb
