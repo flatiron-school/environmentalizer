fc6db6afaee416c7437049e48c5b05ade5eb2589
Logan Hasson <logan.hasson@gmail.com>
2015-07-08 11:22:17 -0400

    Update rvm check

--- shared/rvm/check.sh
+++ shared/rvm/check.sh
@@ -2,10 +2,12 @@
 
 if [[ -f $HOME/.rvm/scripts/rvm ]]; then
   . $HOME/.rvm/scripts/rvm
+
+  rvm get head
 fi
 
 if [[ -n "$(which rvm)" ]] && [[ -f "$HOME/.rvm/bin/rvm" ]] && \
-   [[ $(rvm --version 2> /dev/null) =~ 1\.2|3.* ]]
+   [[ $(rvm --version 2> /dev/null) =~ 1\.26.* ]]
 then
   echo '1'
 else
