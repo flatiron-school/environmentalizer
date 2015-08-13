5415b6294bfa7de1555b1752ed7f5dfff4f5bde6
Logan Hasson <logan.hasson@gmail.com>
2015-08-13 11:06:37 -0400

    revert ruby install

--- shared/ruby/install.sh
+++ shared/ruby/install.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Script *must* be run using sudo
 echo 'Installing Ruby...'
 
 if [[ -f $HOME/.rvm/scripts/rvm ]]; then
@@ -8,7 +7,19 @@ if [[ -f $HOME/.rvm/scripts/rvm ]]; then
   rvm get head
 fi
 
-rvm install ruby-2.2.2
+if [[ $# -eq 0 ]]
+then
+  echo "Must supply password"
+  exit 1
+fi
+
+expect <<-DONE
+spawn rvm install ruby-2.2.2
+expect -re "password required for"
+send "$1\n"
+expect eof
+DONE
+
 rvm use 2.2.2 --default
 
 echo -e "\033[34;32mDone.\033[0m"
