9b4f3b486ad33350aad5f9d36e2358c44be1e732
Logan Hasson <logan.hasson@gmail.com>
2015-08-24 11:20:52 -0400

    Fix rvm check

--- shared/rvm/check.sh
+++ shared/rvm/check.sh
@@ -3,13 +3,17 @@
 if [[ -f $HOME/.rvm/scripts/rvm ]]; then
   . $HOME/.rvm/scripts/rvm
 
-  rvm get head
+  rvm get head > /dev/null 2>&1
 fi
 
-if [[ -n "$(which rvm)" ]] && [[ -f "$HOME/.rvm/bin/rvm" ]] && \
-   [[ $(rvm --version 2> /dev/null) =~ 1\.26.* ]]
+if [[ -n "$(which rvm)" ]] && [[ -f "$HOME/.rvm/bin/rvm" ]]
 then
-  echo '1'
+  if [[ $(rvm --version 2> /dev/null) =~ 1\.26.* ]]
+  then
+    echo '1'
+  else
+    echo '0'
+  fi
 else
   echo '0'
 fi
