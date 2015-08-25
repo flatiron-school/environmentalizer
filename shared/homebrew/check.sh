1e1bd062bd13d6ee16e64cbae8be7ad4d10b9596
Logan Hasson <logan.hasson@gmail.com>
2015-08-25 11:17:38 -0400

    Lighten up on the homebrew check

--- shared/homebrew/check.sh
+++ shared/homebrew/check.sh
@@ -2,7 +2,7 @@
 
 function is_homebrew_installed {
   if [[ $(/usr/bin/which brew) =~ ^/usr/local/bin/brew ]] && \
-     [[ $(/usr/local/bin/brew --version 2>/dev/null) =~ ^0\.9\.[5-9]$ ]]
+     [[ $(/usr/local/bin/brew --version 2>/dev/null) =~ ^0\.9\.[5-9] ]]
   then
     echo '1'
   else
