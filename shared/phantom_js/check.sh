c79d6ca5923279f1d6fd6f854733dcbf1e0a81b4
Logan Hasson <logan.hasson@gmail.com>
2015-10-09 11:59:42 -0400

    Temporarily say PhantomJS is installed for everybody

--- shared/phantom_js/check.sh
+++ shared/phantom_js/check.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-if [ -n "$(brew ls --versions phantomjs 2> /dev/null)" ] && \
-   [[ $(which phantomjs) =~ ^/usr/local/bin/phantomjs$ ]]
-then
-  echo '1'
-else
-  echo '0'
-fi
+#if [ -n "$(brew ls --versions phantomjs 2> /dev/null)" ] && \
+#   [[ $(which phantomjs) =~ ^/usr/local/bin/phantomjs$ ]]
+#then
+#  echo '1'
+#else
+#  echo '0'
+#fi
+
+echo '1'
