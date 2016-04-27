e250054895651be0dc45c5a6cf071621287da262
Logan Hasson <logan.hasson@gmail.com>
2016-04-27 11:55:50 -0400

    Allow git 2.8

--- shared/git/check.sh
+++ shared/git/check.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ -n "$(brew ls --versions git 2> /dev/null)" ]] &&\
-   [[ "$(brew ls --versions git 2> /dev/null)" =~ ^.*2\.4|5|6|7\..*$ ]]
+   [[ "$(brew ls --versions git 2> /dev/null)" =~ ^.*2\.4|5|6|7|8\..*$ ]]
 then
   echo '1'
 else
